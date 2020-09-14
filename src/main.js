8eb887915dce7306857296bca8544a46330af89a
androllen <androllen@hotmail.com>
2020-09-14 16:15:02 +0800

    install element-ui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
+import Element from 'element-ui'
 import App from './App.vue'
+// import 'element-ui/lib/theme-chalk/index.css';
 
+
+Vue.use(Element)
 Vue.config.productionTip = false
 
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  render: h => h(App)
+});
