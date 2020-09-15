634549bec9c5bbd85ac2aedc91836d935d841538
androllen <androllen@hotmail.com>
2020-09-15 18:40:23 +0800

    update

--- src/components/BaseMenu.vue
+++ src/components/BaseMenu.vue
@@ -2,7 +2,7 @@
 <div id="ele_div">
   <el-row class="tac">
     <el-col :span="24">
-      <el-menu router="true" default-active="$route.path" unique-opened="true" class="el-menu-vertical">
+      <el-menu router="true" default-active="/info_cdn" unique-opened="true" class="el-menu-vertical" @open="handleOpen">
         <el-submenu index="1">
           <template slot="title">
             <i class="el-icon-location"></i>
@@ -11,10 +11,10 @@
           <el-menu-item index="/info_cdn">Cdn 检测</el-menu-item>
           <el-menu-item index="/info_waf">Waf 检测</el-menu-item>
           <el-menu-item index="/info_email">邮箱地址收集</el-menu-item>
-          <el-menu-item index="3">端口扫描</el-menu-item>
-          <el-menu-item index="4">子域名爆破</el-menu-item>
-          <el-menu-item index="6">Web 路径扫描</el-menu-item>
-          <el-menu-item index="7">WhoIs 反差</el-menu-item>
+          <el-menu-item index="/info_port">端口扫描</el-menu-item>
+          <el-menu-item index="/info_subdomain">子域名爆破</el-menu-item>
+          <el-menu-item index="/info_webpath">Web 路径扫描</el-menu-item>
+          <el-menu-item index="/info_whois">WhoIs 反查</el-menu-item>
         </el-submenu>
         <el-submenu index="2">
           <template slot="title">
@@ -44,8 +44,22 @@
 
 <script>
 export default {
+
   methods: {
-  
+    // https://www.jianshu.com/p/ecbbd6810f7f
+    // handleSelect(key, keyPath) {
+    //   this.$router.push({
+    //     path: key,
+    //     params: {
+    //       data: 'query'
+    //     }
+    //   })
+    // }
+    handleOpen(key, keyPath) {
+
+      // this.$router.push({path:"/info_cdn"});
+      console.log(key, keyPath);
+    }
 
   }
 }
