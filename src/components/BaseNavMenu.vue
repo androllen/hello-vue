7f44bd776cd0cce3c011a149232a3cf50c918d2e
androllen <androllen@hotmail.com>
2020-09-15 12:05:01 +0800

    el-menu 还需要在深入了解

--- src/components/BaseNavMenu.vue
+++ src/components/BaseNavMenu.vue
@@ -1,28 +1,34 @@
 <template>
-<div>
+<div id="ele_div">
   <el-row class="tac">
-    <el-col :span="12">
-      <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+    <el-col :span="24">
+      <el-menu default-active="1" unique-opened="true" class="el-menu-vertical" @open="handleOpen" @close="handleClose">
         <el-submenu index="1">
-          <template slot="title">
+          <template slot="title" >
             <i class="el-icon-location"></i>
             <span>导航一</span>
           </template>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
+          <el-menu-item index="1">选项1</el-menu-item>
+          <el-menu-item index="2">选项2</el-menu-item>
+          <el-menu-item index="3">选项3</el-menu-item>
+          <el-menu-item index="4">选项4</el-menu-item>
         </el-submenu>
-        <el-submenu>
+        <el-submenu index="2">
           <template slot="title">
             <i class="el-icon-menu"></i>
             <span>导航二</span>
           </template>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
+          <el-menu-item index="1">选项1</el-menu-item>
+          <el-menu-item index="2">选项2</el-menu-item>
+          <el-menu-item index="3">选项3</el-menu-item>
+          <el-menu-item index="4">选项4</el-menu-item>
         </el-submenu>
-        <el-submenu>
+        <el-submenu index="3">
           <template slot="title">
             <i class="el-icon-document"></i>
-            <span>导航二</span>
+            <span>导航三</span>
           </template>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
+          <el-menu-item index="3">选项1</el-menu-item>
         </el-submenu>
       </el-menu>
     </el-col>
@@ -43,3 +49,15 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+#ele_div {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: green;
+  text-align: left;
+}
+
+
+</style>
